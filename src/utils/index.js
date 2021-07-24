0e6e780b4b74f2c631742252e3b5cfea9ddb9087
StevieTaylor <galei111@126.com>
2021-07-25 00:19:05 +0800

    [feat] deepCopy

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,33 @@
 /*
  * @Author: Stevie
  * @Date: 2021-07-17 18:05:16
- * @LastEditTime: 2021-07-17 18:08:56
+ * @LastEditTime: 2021-07-24 20:54:53
  * @LastEditors: Stevie
  * @Description:
  */
+/**
+ * @description: 字符串首字母大写
+ * @param {*} str
+ * @return {*}
+ */
 export function firstUpperCase(str) {
   return str.replace(/( |^)[a-z]/g, (L) => L.toUpperCase())
+}
+
+/**
+ * @description: 简易版深拷贝
+ * @param {*} target
+ * @return {*}
+ */
+export function deepCopy(target) {
+  if (typeof target !== 'object') {
+    return
+  }
+  let result = target instanceof Array ? [] : {}
+  for (const key in target) {
+    if (target.hasOwnProperty(key)) {
+      target[key] = typeof target[key] === 'object' ? deepCopy(target[key]) : target[key]
+    }
+  }
+  return result
 }
