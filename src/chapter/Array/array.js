9dcec689f0fe173d337719ed2ff39cfc0d361186
StevieTaylor <galei111@126.com>
2021-07-28 16:26:25 +0800

    [feat] Array的实现

--- src/chapter/Array/array.js
+++ src/chapter/Array/array.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Stevie
  * @Date: 2021-07-19 11:23:38
- * @LastEditTime: 2021-07-27 00:20:57
+ * @LastEditTime: 2021-07-28 16:25:25
  * @LastEditors: Stevie
  * @Description:
  */
@@ -39,29 +39,49 @@ class ArrayList {
   }
 
   /**
+   * @description:
    *  * 插入元素，有几种情形：
-   *  * 1. 当 size = capacity时，数组满了，属于超范围插入
-   *  * 2. 当 size < capacity时，再比较size和index
-   *  *    2.1. 当 index < 0 时，超范围
-   *  *    2.2. 当 index = 0 时，属于头部插入
-   *  *    2.3. 当 0 < index < size时，属于中间插入
-   *  *    2.4. 当 index = size时，属于尾部插入
-   *  *    2.5. 当 index > size时，超范围
+   *  *  1. 当 size = capacity时，数组满了，属于超范围插入
+   *  *  2. 当 size < capacity时，再比较size和index
+   *  *     2.1. 当 index < 0 时，超范围
+   *  *     2.2. 当 index = 0 时，属于头部插入
+   *  *     2.3. 当 0 < index < size时，属于中间插入
+   *  *     2.4. 当 index = size时，属于尾部插入
+   *  *     2.5. 当 index > size时，超范围
+   * @param {*} index
+   * @param {*} element
+   * @return {*}
    */
   insert(index, element) {
     if (index < 0 || index > this.size) {
       throw new Error('超出数组元素实际范围')
     }
-
+    // 当实际元素大于等于容量时, 需要扩容
     if (this.size >= this.list.length) {
-      this.resize()
-      console.log(this.list)
+      this.list.length += 1
     }
     for (let i = this.size - 1; i >= index; i--) {
       this.list[i + 1] = this.list[i]
     }
     this.list[index] = element
     this.size++
+  }
+
+  /**
+   * @description:
+   * @param {*} index
+   * @return {*}
+   */
+  delete(index) {
+    if (index < 0 || index >= this.size) {
+      throw new Error('超出数组元素实际范围')
+    }
+    const deleteElement = this.list[index]
+    for (let i = index; i < this.size; i++) {
+      this.list[i] = this.list[i + 1]
+    }
+    this.size--
+    return deleteElement
   }
 
   unshift(element) {
@@ -73,12 +93,4 @@ class ArrayList {
   }
 }
 
-const array = new ArrayList(10)
-for (let i = 0; i < array.Capacity; i++) {
-  array.set(i, `e${i}`)
-}
-console.log(`array:`, array.list)
-console.log(`预设容量:`, array.Capacity)
-console.log(`实际大小:`, array.Size)
-array.insert(3, 'insert element')
-console.log(`array`, array.list)
+export default ArrayList
--- src/chapter/index.js
+++ src/chapter/index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: Stevie
  * @Date: 2021-07-09 11:14:00
- * @LastEditTime: 2021-07-27 00:22:26
+ * @LastEditTime: 2021-07-28 16:25:46
  * @LastEditors: Stevie
  * @Description: 静态导入js文件
  */
-// import './Array/array.js'
+// import './Array/use-array'
