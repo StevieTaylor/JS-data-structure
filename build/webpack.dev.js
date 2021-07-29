9e2b70b63fcb07472a7cb72ea283fe66c33467d0
StevieTaylor <galei111@126.com>
2021-07-29 15:56:55 +0800

    [feat] 开发和生产环境配置最佳source-map

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,27 +1,26 @@
 /*
  * @Author: Stevie
  * @Date: 2021-06-14 23:10:50
- * @LastEditTime: 2021-07-08 17:48:11
+ * @LastEditTime: 2021-07-29 15:55:49
  * @LastEditors: Stevie
- * @Description: 
+ * @Description:
  */
-const path = require('path');
-const webpack = require('webpack');
-const {merge} = require('webpack-merge');
+const path = require('path')
+const webpack = require('webpack')
+const { merge } = require('webpack-merge')
 const base = require('./webpack.base')
 
 const devConfig = {
   mode: 'development',
-  devServer:{
-    contentBase: path.resolve(__dirname,'../dist'),
+  devServer: {
+    contentBase: path.resolve(__dirname, '../dist'),
     port: 7080,
     host: 'localhost',
-    compress:false,
-    open: true
+    compress: false,
+    open: true,
   },
-  plugins:[
-    new webpack.HotModuleReplacementPlugin()
-  ]
+  devtool: 'eval-cheap-module-source-map',
+  plugins: [new webpack.HotModuleReplacementPlugin()],
 }
 
-module.exports = merge(base,devConfig);
+module.exports = merge(base, devConfig)
--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,28 +1,29 @@
 /*
  * @Author: Stevie
  * @Date: 2021-06-14 23:11:02
- * @LastEditTime: 2021-06-14 23:37:23
+ * @LastEditTime: 2021-07-29 15:56:01
  * @LastEditors: Stevie
- * @Description: 
+ * @Description:
  */
-const merge = require('webpack-merge');
-const base = require('./webpack.base');
+const base = require('./webpack.base')
+const { merge } = require('webpack-merge')
 const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin')
 
 const prodConfig = {
   mode: 'production',
-  plugins:[
+  devtool: 'hidden-source-map',
+  plugins: [
     new ParallelUglifyPlugin({
       test,
       include,
       exclude,
       cacheDir,
       sourceMap,
-      uglifyJS:{
-        warning:false
-      }
-    })
-  ]
+      uglifyJS: {
+        warning: false,
+      },
+    }),
+  ],
 }
 
-module.exports = merge(base,prodConfig);
+module.exports = merge(base, prodConfig)
