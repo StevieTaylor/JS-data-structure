6e85442f3c249a024413af254370cf3289de563a
StevieTaylor <galei111@126.com>
2021-12-27 17:28:00 +0800

    [feat] import jest

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: Stevie
+ * @Date: 2021-12-27 17:26:27
+ * @LastEditTime: 2021-12-27 17:26:27
+ * @LastEditors: Stevie
+ * @Description:
+ */
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+}
