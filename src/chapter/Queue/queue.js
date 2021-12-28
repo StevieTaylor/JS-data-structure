122462aa210e3591a18e2562bf9f49135cd35d26
StevieTaylor <galei111@126.com>
2021-12-28 16:31:00 +0800

    [feat] Queue

---
+++ src/chapter/Queue/queue.js
@@ -0,0 +1,46 @@
+/*
+ * @Author: Stevie
+ * @Date: 2021-12-27 17:42:34
+ * @LastEditTime: 2021-12-28 16:23:18
+ * @LastEditors: Stevie
+ * @Description:
+ */
+export default class Queue {
+  constructor() {
+    this.elements = []
+  }
+
+  // * 入队
+  enqueue(element) {
+    this.elements.push(element)
+  }
+
+  // * 出队
+  dequeue() {
+    if (this.isEmpty) {
+      return
+    }
+    this.elements.shift()
+  }
+
+  // * 清空队列
+  clear() {
+    this.elements = []
+  }
+
+  // * 返回队列第一个元素
+  get front() {
+    if (this.isEmpty) {
+      return
+    }
+    return this.elements[0]
+  }
+
+  get size() {
+    return this.elements.length
+  }
+
+  get isEmpty() {
+    return this.elements.length === 0
+  }
+}
---
+++ src/chapter/Queue/queue.test.js
@@ -0,0 +1,65 @@
+/*
+ * @Author: Stevie
+ * @Date: 2021-12-28 15:45:48
+ * @LastEditTime: 2021-12-28 16:30:18
+ * @LastEditors: Stevie
+ * @Description:
+ */
+import Queue from './queue'
+
+describe('Queue', () => {
+  it('should be defined', () => {
+    expect(Queue).toBeDefined()
+  })
+
+  let queue
+
+  beforeEach(() => {
+    queue = new Queue()
+  })
+
+  afterEach(() => {
+    queue = null
+  })
+
+  it('should be empty', () => {
+    expect(queue.isEmpty).toBeTruthy()
+  })
+
+  it('should support enqueue', () => {
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+    expect(queue.elements).toEqual([1, 2, 3])
+  })
+
+  it('should support dequeue', () => {
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+    queue.dequeue()
+    expect(queue.elements).toEqual([2, 3])
+  })
+
+  it('should support front', () => {
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+    expect(queue.front).toBe(1)
+  })
+
+  it('should support clear', () => {
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+    queue.clear()
+    expect(queue.elements).toEqual([])
+  })
+
+  it('should support size', () => {
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+    expect(queue.size).toBe(3)
+  })
+})
